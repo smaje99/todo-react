4d89d3ecb7b8420f5d6dcddbdc1224679fd55ab9
Sergio Majé <smajefranco@gmail.com>
2021-05-02 22:10:16 -0500

    key of TodoItem

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import { TodoItem } from './TodoItem'
 
 export function TodoList({ todos }) {
-    return (<ul>
-        {todos.map((todo) => {;
-            <TodoItem todo={todo} />
-        })}
-    </ul>);
+    return (
+        <ul>
+            {todos.map((todo) => {;
+                <TodoItem key={todo.id} todo={todo} />
+            })}
+        </ul>
+    );
 }
