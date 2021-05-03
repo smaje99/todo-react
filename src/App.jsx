82f5bcd04b537878ffb5588e4b73758642bdca8e
Sergio Majé <smajefranco@gmail.com>
2021-05-02 22:08:50 -0500

    Add Events & useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
+import {v4 as uuid } from 'uuid/v4'
 
 import { TodoList } from './components/TodoList'
 
 export function App() {
     const [todos, setTodos] = useState([
         { id: 1, task: 'Tarea 1', completed: false }
-    ])
+    ]);
+
+    const todoTaskRef = useRef();
+
+    const handleTodoAdd = () => {
+        const task = todoTaskRef.current.value;
+        task !== '' && setTodos((prevTodos) => {
+            return [...prevTodos, { id: uuid(), task, completed: false }]
+        });
+        todoTaskRef.current.value = null;
+    };
 
     return (
         <>
             <TodoList todos={todos} />
-            <input type="text" placeholder="Nueva Tarea" />
-            <button>➕</button>
-            <button>🗑</button>
+            <input ref={todoTaskRef} type="text" placeholder="Nueva Tarea" />
+            <button onClick={handleTodoAdd}>➕</button>
+            <button onClick={}>🗑</button>
         </>
     );
 }
