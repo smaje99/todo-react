c58deac96ebbce335802b62ceebbc39753a4c594
Sergio Majé <smajefranco@gmail.com>
2021-05-10 00:47:26 -0500

    Todo Item styles

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import '../styles/TodoItem.css'
+
 export function TodoItem({ todo, toggleTodo }) {
     const { id, task, completed } = todo
 
@@ -7,12 +9,16 @@ export function TodoItem({ todo, toggleTodo }) {
 
     return(
         <li>
-            <input
-                type="checkbox"
-                checked={completed}
-                onChange={handleTodoClick}
-            />
-            {task}
+            <div className="item">
+                <input
+                    type="checkbox"
+                    id={id}
+                    className="item__checked"
+                    checked={completed}
+                    onChange={handleTodoClick}
+                />
+                <label for={id} className="item__task">{task}</label>
+            </div>
         </li>
     );
 }
