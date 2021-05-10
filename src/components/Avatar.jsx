d8c679eca8b736f764ff6cba86f130dff920a128
Sergio Majé <smajefranco@gmail.com>
2021-05-09 21:02:48 -0500

    Avatar styles

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import '../styles/Avatar.css'
+
 export function Avatar() {
     return (
         <aside className="avatar">
-            <img src="images/avatar.svg" alt="avatar" />
-            <div className="avatar__message">
-                <p>
+            <img className="avatar__image" src="images/avatars.svg" alt="avatar" />
+            <div className="avatar__balloon">
+                <p className="avatar__balloon__message">
                     ¿Qué hay para hacer hoy?
                 </p>
             </div>
