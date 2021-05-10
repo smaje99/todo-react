5087dcb54b387b2e777a5b45c9b5d7d46cce64f4
Sergio Majé <smajefranco@gmail.com>
2021-05-09 20:03:05 -0500

    App refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Todo } from '../components/Todo'
-import { Avatar } from '../components/Avatar'
+import { Todo } from './components/Todo'
+import { Avatar } from './components/Avatar'
 
 export function App() {
     return (
