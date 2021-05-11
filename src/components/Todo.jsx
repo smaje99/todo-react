43480e0f29dec4dd7e34c04c164164cfc1984db3
Sergio Majé <smajefranco@gmail.com>
2021-05-11 14:09:43 -0500

    Components refactoring

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,61 +1,10 @@
-import React, { useState, useRef, useEffect } from 'react'
-
-import uuid from 'react-uuid'
+import React from 'react'
 
 import { TodoList } from './TodoList'
 
 import '../styles/Todo.css'
 
-const KEY = 'todoApp.todos'
-
-export function Todo() {
-    const [todos, setTodos] = useState([
-        { id: uuid(), task: 'Tarea 1', completed: false }
-    ]);
-
-    const [left, setLeft] = useState(0)
-
-    const todoTaskRef = useRef();
-
-    useEffect(() => {
-        const storedTodos = JSON.parse(localStorage.getItem(KEY));
-        storedTodos && setTodos(storedTodos);
-    }, []);
-
-    useEffect(() => {
-        localStorage.setItem(KEY, JSON.stringify(todos));
-    }, [todos]);
-
-    useEffect(() => {
-        setLeft(todos.filter((todo) => !todo.completed).length)
-    }, [todos]);
-
-    useEffect(() => {
-        todoTaskRef.current.focus();
-    });
-
-    const toggleTodo = (id) => {
-        const newTodos = [...todos];
-        const todo = newTodos.find((todo) => todo.id === id);
-        todo.completed = !todo.completed;
-        setTodos(newTodos);
-    };
-
-    const handleClearAll = () => {
-        const newTodos = todos.filter((todo) => !todo.completed);
-        setTodos(newTodos);
-    };
-
-    const onEnterPress = (event) => {
-        if (event.key === 'Enter') {
-            const task = todoTaskRef.current.value.trim();
-            task !== '' && setTodos((prevTodos) => {
-                return [...prevTodos, { id: uuid(), task, completed: false }]
-            });
-            todoTaskRef.current.value = null;
-        }
-    }
-
+export function Todo({ todos, toggleTodo, todoTaskRef, onEnterPress }) {
     return (
         <section className="todo">
             <TodoList todos={todos} toggleTodo={toggleTodo} />
@@ -66,14 +15,6 @@ export function Todo() {
                 placeholder="Nueva Tarea"
                 onKeyPress={onEnterPress}
             />
-            <div className="todo__left">
-                <p className="todo__left__text">
-                    Te quedan <span className="left">{left}</span> tareas por terminar
-                </p>
-            </div>
-            <button className="todo__clear" onClick={handleClearAll}>
-                Limpiar tareas
-            </button>
         </section>
     );
 }
