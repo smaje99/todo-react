da818bca38ec00975a4ef137960766473f27713f
Sergio Majé <smajefranco@gmail.com>
2021-05-13 14:36:07 -0500

    Avatar image refactoring

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import avatars from '../images/avatars.svg'
 import '../styles/Avatar.css'
 
 export function Avatar() {
     return (
         <aside className="avatar">
-            <img className="avatar__image" src="images/avatars.svg" alt="avatar" />
+            <img className="avatar__image" src={avatars} alt="avatar" />
             <div className="avatar__balloon">
                 <span className="avatar__balloon__message">
                     ¿Qué hay para hacer hoy?
